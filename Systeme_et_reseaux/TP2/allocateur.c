de0471fd61904bd1f8e4e96e178fd04a0e759f1a
Maël <ext-mael.archenault@aalto.fi>
2025-09-24 11:53:51 +0200

    Making clear test functions

--- Systeme_et_reseaux/TP2/allocateur.c
+++ Systeme_et_reseaux/TP2/allocateur.c
@@ -6,16 +6,16 @@
 
 
 typedef struct HEADER_TAG {
-    struct HEADER_TAG * ptr_next; /* pointe sur le prochain bloc libre */
-    size_t block_size; /* taille du free_memory_list bloc en octets*/
-    long magic_number; /* 0x0123456789ABCDEFL */
+    struct HEADER_TAG * ptr_next; 
+    size_t block_size; 
+    long magic_number; 
 } HEADER;
 
 
 HEADER* free_memory_list = NULL;
 
 
-void* mallloc_3is(size_t block_size){
+void* malloc_3is(size_t block_size){
     HEADER* current = free_memory_list;
     HEADER* previous = NULL;
 
@@ -37,6 +37,11 @@ void* mallloc_3is(size_t block_size){
             current->ptr_next = NULL;
             current->block_size = block_size;
 
+
+            *((char*)current + sizeof(HEADER) + block_size) = MAGIC_NUMBER;
+
+            return (void*)(current +1);
+
             
         }
         previous = current;
@@ -53,12 +58,23 @@ void* mallloc_3is(size_t block_size){
     new->ptr_next = NULL;
     new->block_size = block_size;
     new->magic_number = MAGIC_NUMBER;
+
+    
+    *((char*)new + sizeof(HEADER) + block_size) = MAGIC_NUMBER;
+
     return (void*)(new+1);
 };
 
 
 void* free_3is(void* ptr){
     HEADER* header = (HEADER*)ptr -1;
+
+    // Magic number Verification (not working)
+
+    // long magic_number = *(long*)((char*)header + sizeof(HEADER) + header->block_size);
+    // if (header->magic_number != magic_number){
+    //     printf("%d\n\r%d\n\r", magic_number, header->magic_number);
+    // };
 
     HEADER* current = free_memory_list;
     HEADER* previous = NULL;
@@ -74,15 +90,19 @@ void* free_3is(void* ptr){
     else{
         previous->ptr_next = header;
     }
+
+    
     header->ptr_next = current;
 }
 
 void display_free_memory_list(){
+    printf("-----Free Memory Blocks ------\n\r");
     HEADER* current = free_memory_list;
     while(current != NULL){
         display_memory_block(current);
         current = current->ptr_next;
     }
+    printf("-----------------------\n\r\n\r");
 }
 
 void display_memory_block(HEADER* head){
@@ -94,35 +114,39 @@ void display_memory_block(HEADER* head){
 
 
 int main(void) {
-    int* tab = mallloc_3is(3*sizeof(int));
+
+    // 1.Multiple allocations
+
+    int* tab1 = malloc_3is(3*sizeof(int));
+    int* tab2 = malloc_3is(3*sizeof(int));
 
     for (int i = 0; i<3; i++){
-        tab[i] = i;
-    }
-    for (int i = 0; i<3; i++){
-        printf("%d\n\r", tab[i]);
-    }
-
-    int* tab2 = mallloc_3is(5*sizeof(int));
-
-    for (int i = 0; i<5; i++){
+        tab1[i] = i;
         tab2[i] = i;
     }
-    for (int i = 0; i<5; i++){
-        printf("%d\n\r", tab2[i]);
+    for (int i = 0; i<4; i++){
+        printf("%d %d\n\r", tab1[i], tab2[i]);
     }
 
-    int* tab3 = mallloc_3is(5*sizeof(int));
-
-    for (int i = 0; i<5; i++){
-        tab3[i] = i;
-    }
-    for (int i = 0; i<5; i++){
-        printf("%d\n\r", tab2[i]);
-    }
+    // 2. Block liberation
 
     free_3is(tab2);
-    free_3is(tab3);
-
     display_free_memory_list();
+
+    // 3. Memory overflow verification
+
+    // Not working
+
+    // 4. Reuse free memory
+
+    int* tab2_bis = malloc_3is(2*sizeof(int));
+    // tab2_bis will reuse the 12 bytes place that was liberated in .2
+    // but it only needs 8 bytes so, the free memory list will have a block of size 4
+    display_free_memory_list();
+
+    // 5. High size blocks cutting
+
+    // Works automatically (see previous question)
+
+
 }
